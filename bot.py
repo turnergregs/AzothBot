d594f46f161074f08fae3aa7f4e2f728a1d4b2e6
turnergregory <tbg435@utexas.edu>
2025-05-07 08:39:43 -0500

    added command scaffolds

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
-# bot.py
 import nextcord
 from nextcord.ext import commands
 from dotenv import load_dotenv
 import os
 
+from azoth_commands import AzothCommands
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = nextcord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(intents=intents)
+# bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
 	print(f"✅ Logged in as {bot.user}")
+	await bot.sync_all_application_commands()
 
-@bot.slash_command(name="ping", description="Check if AzothBot is online")
-async def ping(interaction: nextcord.Interaction):
-	await interaction.response.send_message("Pong!")
-
+bot.add_cog(AzothCommands(bot))
 bot.run(TOKEN)
